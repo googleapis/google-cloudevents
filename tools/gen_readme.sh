fb2c730cf70e5975d4293864da87c55faac791e3
Grant Timmerman <timmerman+devrel@google.com>
2020-07-09 15:51:40 -0700

    docs: improve readme gen script
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- tools/gen_readme.sh
+++ tools/gen_readme.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 cd ..
 for f in $(find . -name 'data.proto' -or -name 'events.proto'); do
-  # echo $f;
-  c=$(grep 'package' $f)
-  # c_no_quotes=`echo "$c" | cut -d'"' -f 2`
+  package_string=$(grep 'package' $f)
 
   # Only print .data files
   STR=$f
   SUB="data.proto"
   if [[ "$STR" == *"$SUB"* ]]; then
     # Trim off text "package " and ";""
-    package=${c:8:${#c}-9}
+    package=${package_string:8:${#package_string}-9}
     echo "- [$package]($f)"
   fi
 done
