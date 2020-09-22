a0f696a6d3372abbc8f9bb49b6614b1cd5b1104c
chenyumic <chenyumic@google.com>
2020-09-23 05:48:51 +0800

    Updated the quicktype-wrapper package (#70)
    * Updated the quicktype-wrapper (qt) package
    
    * Added docstring

--- tools/quicktype-wrapper/src/index.ts
+++ tools/quicktype-wrapper/src/index.ts
@@ -18,19 +18,6 @@ const IN = argv.in || process.env.IN;
 const OUT = argv.out || process.env.OUT;
 const L = (argv.l || process.env.L || LANGUAGE.TYPESCRIPT).toUpperCase() as TARGET_LANGUAGE;
 
-console.log(
-`***********************
-** Quicktype Wrapper **
-***********************
-* Valid Languages (L): ${Object.values(LANGUAGE)}
-***********************
-* Config:
-- IN=${IN}
-- OUT=${OUT}
-- L=${L}
-***********************
-`);
-
 // The name of the JSON Schema file
 const SCHEMA_FILE = 'schema.json';
 
@@ -45,46 +32,88 @@ async function getJSONSchemasPaths(directory: string) {
   return paths;
 }
 
-// Start the program
-(async () => {
-  console.log('== START ==');
-  // Validate configuration
-  if (!IN) console.error('Error in config: `IN` not set');
-  if (!OUT) console.error('Error in config: `OUT` not set');
-  if (!IN || !OUT) return;
+/**
+ * Gets a list of tuples of all JSON schemas and code generated from them
+ * @param directory The path to the directory with schemas.
+ * @param language The language of the code.
+ */
+export async function getJSONSchemasAndGenFiles(directory: string, language: string) {
+  const absPaths = await getJSONSchemasPaths(directory);
 
-  // Get all paths for input.
-  const absPaths = await getJSONSchemasPaths(IN);
-  const relPaths = absPaths.map((p: string) => {
-    return p.substr(IN.length);
-  });
-  console.log(`Found ${absPaths.length} schema(s).`);
+  let schemasAndGenFiles: Array<[object, string]> = [];
 
-  // Loop through every path
-  const pathPromises = absPaths.map(async (f: string, i: number) => {
-    const file = await readFileSync(f) + '';
-    const relPath = relPaths[i];
-    const typeName = JSON.parse(file).name;
-
-    // Gather the file paths
-    const mkdirpRelPath = relPath.substr(0, relPath.length - SCHEMA_FILE.length); // contains '/'
-    const outFolder = `${OUT}/${mkdirpRelPath}`; // foo/bar/
-    const outFile = `${typeName}.${LANGUAGE_EXT[L]}`; // PubSubData.java
-    const outPath = `${outFolder}${outFile}`; // foo/bar/PubSubData.java
-
-    console.log(`- ${typeName.padEnd(40, ' ')}: ${mkdirpRelPath}${outFile}`);
-
-    // Generate language using quicktype
+  const promises = absPaths.map(async (f: string, i: number) => {
+    const file = readFileSync(f) + '';
+    const schema = JSON.parse(file);
     const genFile = await jsonschema2language({
       jsonSchema: file,
-      language: L,
-      typeName,
-    });
-    
-    // Make the directory
-    await mkdirp(outFolder);
-    writeFileSync(outPath, genFile);
+      language: language,
+      typeName: "Event"
+    })
+
+    schemasAndGenFiles.push([schema, genFile]);
   });
-  await Promise.all(pathPromises);
-  console.log('== END ==');
-})();
+
+  await Promise.all(promises);
+
+  return schemasAndGenFiles;
+}
+
+// Start the program
+if (!module.parent) {
+  (async () => {
+    console.log(
+    `***********************
+    ** Quicktype Wrapper **
+    ***********************
+    * Valid Languages (L): ${Object.values(LANGUAGE)}
+    ***********************
+    * Config:
+    - IN=${IN}
+    - OUT=${OUT}
+    - L=${L}
+    ***********************
+    `);
+
+    console.log('== START ==');
+    // Validate configuration
+    if (!IN) console.error('Error in config: `IN` not set');
+    if (!OUT) console.error('Error in config: `OUT` not set');
+    if (!IN || !OUT) return;
+
+    // Get all paths for input.
+    const absPaths = await getJSONSchemasPaths(IN);
+    const relPaths = absPaths.map((p: string) => {
+      return p.substr(IN.length);
+    });
+    console.log(`Found ${absPaths.length} schema(s).`);
+
+    // Loop through every path
+    const pathPromises = absPaths.map(async (f: string, i: number) => {
+      const file = await readFileSync(f) + '';
+      const relPath = relPaths[i];
+      const typeName = JSON.parse(file).name;
+
+      // Gather the file paths
+      const mkdirpRelPath = relPath.substr(0, relPath.length - SCHEMA_FILE.length); // contains '/'
+      const outFolder = `${OUT}/${mkdirpRelPath}`; // foo/bar/
+      const outFile = `${typeName}.${LANGUAGE_EXT[L]}`; // PubSubData.java
+      const outPath = `${outFolder}${outFile}`; // foo/bar/PubSubData.java
+
+      console.log(`- ${typeName.padEnd(40, ' ')}: ${mkdirpRelPath}${outFile}`);
+
+      // Generate language using quicktype
+      const genFile = await jsonschema2language({
+        jsonSchema: file,
+        language: L,
+        typeName,
+      });
+      
+      // Make the directory
+      await mkdirp(outFolder);
+      writeFileSync(outPath, genFile);
+    });
+    await Promise.all(pathPromises);
+    console.log('== END ==');
+  })();
+};
