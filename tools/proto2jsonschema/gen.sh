ac9f8f3681ccf3b487a37c2b7f614b3c90d0bcf3
Grant Timmerman <timmerman+devrel@google.com>
2021-09-27 09:57:18 -0700

    feat!: use $ref for all jsonschemas
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- tools/proto2jsonschema/gen.sh
+++ tools/proto2jsonschema/gen.sh
@@ -54,7 +54,7 @@ curl -sSL \
 unzip -q protobuf.zip
 cd ..
 
-PROTOC_PLUGIN_VERSION=1.0.0
+PROTOC_PLUGIN_VERSION=investigating_ref_behaviour
 echo "- Setting up protoc plugin: chrusty/protoc-gen-jsonschema@$PROTOC_PLUGIN_VERSION"
 # Pin chrusty tool to specific version: https://github.com/chrusty/protoc-gen-jsonschema/tags
 GO111MODULE=on go get -v github.com/chrusty/protoc-gen-jsonschema/cmd/protoc-gen-jsonschema@$PROTOC_PLUGIN_VERSION
@@ -105,7 +105,7 @@ echo "- Done with gen."
 
 # Postgen
 echo "- Postgen: Start"
-cd $(dirname $0)
-npm i
-node postgen.js
+# cd $(dirname $0)
+# npm i
+# node postgen.js
 echo "- Postgen: End"
